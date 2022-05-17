cc7b0538f1beace6275bb680c2a88a0b1f9a71a8
athul-23p <athul238p@gmail.com>
2022-05-17 12:22:03 +0530

    - ui improvements

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const initState = [
     first_name: "Annete",
     last_name: "Murphy",
     phone: "9876541230",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://www.themobileindian.com/wp-content/uploads/2021/06/facebook-avatar-main.jpg",
     email: "sara.cruz@example.com",
   },
   {
@@ -14,7 +14,7 @@ const initState = [
     first_name: "Theresa",
     last_name: "Watson",
     phone: "5479236412",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://www.cnet.com/a/img/resize/19b841ad4e179a5835c1cfbe664649412b4a7d9c/2020/05/18/5bac8cc1-4bd5-4496-a8c3-66a6cd12d0cb/fb-avatar-2.jpg?auto=webp&width=940",
     email: "michael.mitchel@example.com",
   },
   {
@@ -22,7 +22,7 @@ const initState = [
     first_name: "Cody",
     last_name: "Howard",
     phone: "8456325416",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://static.vecteezy.com/system/resources/thumbnails/002/002/247/small_2x/beautiful-black-woman-avatar-character-icon-free-vector.jpg",
     email: "glen.lane@example.com",
   },
   {
@@ -30,7 +30,7 @@ const initState = [
     first_name: "Max",
     last_name: "Lane",
     phone: "6874592604",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://www.nj.com/resizer/zovGSasCaR41h_yUGYHXbVTQW2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg",
     email: "dwight.fleming@example.com",
   },
   {
@@ -38,7 +38,7 @@ const initState = [
     first_name: "Marvin",
     last_name: "Fisher",
     phone: "7643269410",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png",
     email: "kelly.howard@example.com",
   },
   {
@@ -46,7 +46,7 @@ const initState = [
     first_name: "Brooklyn",
     last_name: "Mccoy",
     phone: "5976321450",
-    img: "https://via.placeholder.com/150/0000FF/808080?Text=profile_pic",
+    img: "https://static.vecteezy.com/system/resources/thumbnails/002/002/363/small_2x/man-avatar-character-isolated-icon-free-vector.jpg",
     email: "chad.stephens@example.com",
   },
 ];
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -2,15 +2,14 @@ import { useState } from 'react';
 import styles from './ContactCard.module.css';
 
 function ContactCard({person}){
-    console.log(person);
+   
     const [status, setStatus] = useState(false);
     const handleClick = ()=> setStatus(!status);
     return (
         <div onClick={handleClick} className={styles.card_container} >
-            {/* <div> */}
-            {/* </div> */}
-            <img src="person.profile_pic" alt="" className={styles.profile_pic} />
-            <div>
+           
+            <img src={person.img} alt="" className={ status ?`${styles.profile_pic} ${styles.img_large}` :styles.profile_pic} />
+            <div className={styles.contact_details}>
                 <p className={styles.name}>{person.first_name}  {person.last_name}</p>
                 <p className={styles.grey}>{person.email}</p>
                 <p className={status?"":styles.hide}>{person.phone}</p>
