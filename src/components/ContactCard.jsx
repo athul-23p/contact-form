9886fc9659b6c4d45ae7e2e1fa19d60f9c44e8b1
athul-23p <athul238p@gmail.com>
2022-05-17 17:47:44 +0530

    - conditional rendering using react

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -6,16 +6,25 @@ function ContactCard({person}){
     const [status, setStatus] = useState(false);
     const handleClick = ()=> setStatus(!status);
     return (
-        <div onClick={handleClick} className={styles.card_container} >
-           
-            <img src={person.img} alt="" className={ status ?`${styles.profile_pic} ${styles.img_large}` :styles.profile_pic} />
-            <div className={styles.contact_details}>
-                <p className={styles.name}>{person.first_name}  {person.last_name}</p>
-                <p className={styles.grey}>{person.email}</p>
-                <p className={status?"":styles.hide}>{person.phone}</p>
-            </div>
+      <div onClick={handleClick} className={styles.card_container}>
+        <img
+          src={person.img}
+          alt=""
+          className={
+            status
+              ? `${styles.profile_pic} ${styles.img_large}`
+              : styles.profile_pic
+          }
+        />
+        <div className={styles.contact_details}>
+          <p className={styles.name}>
+            {person.first_name} {person.last_name}
+          </p>
+          <p className={styles.grey}>{person.email}</p>
+          {status && <p>{person.phone}</p>}
         </div>
-    )
+      </div>
+    );
 }
 
 export default ContactCard;
